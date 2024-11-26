7d75a49b98cfa28c20ae69a6677d600ffec59735
RoggerMen <130023813+RoggerMen@users.noreply.github.com>
2024-11-26 18:24:21 -0500

    Consumo de Api con fetch Pinta los datos por medio de props en Card y los muestra en Home

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,18 +1,19 @@
-
-const Card = () => {
+/* eslint-disable react/prop-types */
+const Card = ({item}) => {
+  const imageUrl = item?.images[0]?.replace(/^\[|"|\]$/g, '');
   return (
-    <div className="bg-white cursor-pointer w-56 h-60 rounded-lg shadow-sm hover:shadow-xl transition-shadow duration-300">
-        <figure className='relative mb-3.5 w-full h-4/5 ' >
-            <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">Electronics</span>
+    <div className="bg-white cursor-pointer w-56 h-60 rounded-lg shadow-sm hover:shadow-xl transition-shadow duration-300 my-2">
+        <figure className='relative mb-1 w-full h-4/5 ' >
+            <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">{item?.category?.name}</span>
             <img className="w-full h-full object-cover rounded-lg"
-            src="https://images.pexels.com/photos/1649771/pexels-photo-1649771.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Headphones" />
+            src={imageUrl} alt={item?.title} />
             <button className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1">
                     +
             </button>
         </figure>
         <p className="flex justify-between">
-            <span className="text-sm font-light">Headphones</span>
-            <span className="text-sm font-medium">$300</span>
+            <span className="text-sm font-light px-2">{item?.title}</span>
+            <span className="text-lg font-semibold px-2 py-1.5">${item.price}</span>
         </p>
     </div>
   )
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,38 @@
+import { useState, useEffect } from "react"
 import Card from "../../Components/Card"
 import Layout from "../../Components/Layout"
 const Home = () => {
+
+  const [items,setItems] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      // fetch data from API
+    const response = await fetch('https://api.escuelajs.co/api/v1/products');
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    setItems(data);
+    } catch (error) {
+      console.log("Al buscar el API hubo este ERROR: " + error );
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <>
       <Layout>
         <h1>Home</h1>
-        <Card />
+        <div className='grid place-items-center gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 w-full max-w-screen-xl'>
+        {
+          items && items?.map((item) => (
+            <Card key={item?.id} item={item} />
+          ))
+        }
+        </div>
       </Layout>
       
     </>
