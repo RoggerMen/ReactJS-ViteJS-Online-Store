29b28f16da9b0c2aec2c2d34338da4406f430988
RoggerMen <130023813+RoggerMen@users.noreply.github.com>
2024-11-28 22:30:11 -0500

    haciendo diseño en OrderCard y Cambiando algo minimo en MyOrders

--- src/Components/OrdersCard/index.jsx
+++ src/Components/OrdersCard/index.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/prop-types */
+import { ChevronRightIcon } from '@heroicons/react/24/solid'
 
 const OrdersCard = (props) => {
   
@@ -6,12 +7,17 @@ const OrdersCard = (props) => {
 
 
   return (
-    <div className="flex justify-between items-center mb-4 border border-black">
-        <p>
-            <span>01.02.25</span>
-            <span>{totalProducts}</span>
-            <span>{totalPrice}</span>
-        </p>
+    <div className="flex justify-between items-center mb-4 border border-black rounded-lg p-4 w-80">
+        <div className="flex justify-between w-full">
+            <p className="flex flex-col">
+                <span className="text-xl font-light">01.02.25</span>
+                <span className="text-xl font-light">{totalProducts} articles</span>
+            </p>
+            <p className='flex items-center gap-2'>
+            <span className="text-2xl font-bold">${totalPrice}</span>
+            <ChevronRightIcon className="size-6 text-blue-950 cursor-pointer"/>
+            </p>
+        </div>
     </div>
   )
 }
