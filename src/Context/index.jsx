07bd81ad718f91522308919d983566bb0a9c33fb
RoggerMen <130023813+RoggerMen@users.noreply.github.com>
2024-11-28 22:51:48 -0500

    Pasamos el useEffect y useState de manera local en el Home a manera de useContext osea Global al COMPONENTE Context le mandamos todo donde este proveera los valores y despues creamos un input con el BUSCADOR Y en el Context agregamos un useState para el buscador

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import { createContext, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
 
 export const ShoppingCartContext = createContext();
 const ShoppingCartContextProvider = ({children}) => {
@@ -40,6 +40,32 @@ const ShoppingCartContextProvider = ({children}) => {
   // Shopping Cart - Order
   const [order,setOrder] = useState([]);
 
+  // GET PRODUCTS
+  const [items,setItems] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      // fetch data from API
+      // ANTERIOR API GET QUE USAMOS
+      // https://api.escuelajs.co/api/v1/products
+      // OTRA API GET MAS LIMPIA https://fakestoreapi.com/products
+    const response = await fetch('https://fakestoreapi.com/products');
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    setItems(data);
+    } catch (error) {
+      console.log("Al buscar el API hubo este ERROR: " + error );
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  // GET PRODUCTS BY TITLE
+  const [searchByTitle,setSearchByTitle] = useState(null);
+
   return (
     <ShoppingCartContext.Provider value={{
         count,
@@ -56,6 +82,10 @@ const ShoppingCartContextProvider = ({children}) => {
         closeCheckoutSideMenu,
         order,
         setOrder,
+        items,
+        setItems,
+        searchByTitle,
+        setSearchByTitle
     }}>
         {children}
     </ShoppingCartContext.Provider>
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,38 +1,32 @@
-import { useState, useEffect } from "react"
+import { useContext } from "react";
 import Card from "../../Components/Card"
 import Layout from "../../Components/Layout"
 import ProductDetail from "../../Components/ProductDetail";
+import { ShoppingCartContext } from "../../Context";
 const Home = () => {
 
-  const [items,setItems] = useState(null);
-
-  const fetchData = async () => {
-    try {
-      // fetch data from API
-      // ANTERIOR API GET QUE USAMOS
-      // https://api.escuelajs.co/api/v1/products
-      // OTRA API GET MAS LIMPIA https://fakestoreapi.com/products
-    const response = await fetch('https://fakestoreapi.com/products');
-    console.log(response);
-    const data = await response.json();
-    console.log(data);
-    setItems(data);
-    } catch (error) {
-      console.log("Al buscar el API hubo este ERROR: " + error );
-    }
-  }
-
-  useEffect(() => {
-    fetchData();
-  }, []);
+  const context = useContext(ShoppingCartContext);
 
   return (
     <>
       <Layout>
-        <h1>Home</h1>
+          <div className="flex items-center justify-center relative w-80 mb-4">
+          
+            <h1 className="font-medium text-3xl">Exclusive Products</h1>
+          </div>
+
+        <input 
+        type="text" 
+        placeholder="Search a Product" 
+        className="rounded-lg border border-blue-950 w-80 p-4 mb-4 focus:outline-none"
+        onChange={(e) =>{
+          context.setSearchByTitle(e.target.value);
+        }}
+        />
+
         <div className='grid place-items-center gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 w-full max-w-screen-xl'>
         {
-          items && items?.map((item) => (
+          context.items && context.items?.map((item) => (
             <Card key={item?.id} item={item} />
           ))
         }
